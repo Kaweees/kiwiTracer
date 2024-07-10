93830517b0e9e83a70c57c40cea064ffa68db7d8
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-07-09 23:38:37 -0700

    Add progess

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Graphics Demo
 
+ImageMagick
+
 ```sh
 clear && cmake CMakeLists.txt && make && ./graphics > ./images/graphics.ppm
+magick ./images/graphics.ppm ./images/graphics.png
 ```
+
+<!-- PROJECT PREVIEW -->
+## Preview
+<p align="center">
+  <img src="images/graphics.png"
+  width = "80%"
+  alt = "Current capabilities of the raytracer."
+  />
+</p>
--- include/ray.hpp
+++ include/ray.hpp
@@ -3,24 +3,24 @@
 #include "../include/vector.hpp"
 
 // Represents a ray in three-dimensional space
-class Ray {
-  public:
-  // Constructor to initialize memory
-  Ray() : origin(0, 0, 0), direction(0, 0, 0) {}
-  Ray(const Vector3D& origin, const Vector3D& direction)
-      : origin(origin), direction(direction) {}
+// class Ray {
+//   public:
+//   // Constructor to initialize memory
+//   Ray() : origin(0, 0, 0), direction(0, 0, 0) {}
+//   Ray(const Vector3D& origin, const Vector3D& direction)
+//       : origin(origin), direction(direction) {}
 
-  // Destructor to free the memory allocated
-  ~Ray() = default;
+//   // Destructor to free the memory allocated
+//   ~Ray() = default;
 
-  // The origin of the ray
-  // A
-  Point3D origin;
-  // The direction of the ray
-  // B
-  Vector3D direction;
+//   // The origin of the ray
+//   // A
+//   Point3D origin;
+//   // The direction of the ray
+//   // B
+//   Vector3D direction;
 
-  // Get the point at a distance t along the ray (parameterized form)
-  // P(t) = A + t * B
-  Point3D at(double t) const { return origin + t * direction; }
-};
+//   // Get the point at a distance t along the ray (parameterized form)
+//   // P(t) = A + t * B
+//   Point3D at(double t) const { return origin + t * direction; }
+// };
