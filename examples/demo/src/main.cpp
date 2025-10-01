2eae0ee6fbf43ade6c3ba137e134ffa6ac488229
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-10-01 14:35:55 -0700

    Rasterizer

--- examples/demo/src/main.cpp
+++ examples/demo/src/main.cpp
@@ -4,19 +4,18 @@
 #include <lyra/lyra.hpp>
 #include <iostream>
 #include <filesystem>
-#include <fstream>
-#include <algorithm>
+#include <sys/types.h>
 
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv) {
   fs::path filename;
-  int width = 0, height = 0;
+  int g_width = 0, g_height = 0;
   int vax = 0, vay = 0, vbx = 0, vby = 0, vcx = 0, vcy = 0;
 
   auto cli = lyra::cli() | lyra::arg(filename, "filename").required().help("Output filename (e.g., foo.png)") |
-             lyra::arg(width, "width").required().help("Image width (e.g., 512)") |
-             lyra::arg(height, "height").required().help("Image height (e.g., 512)") |
+             lyra::arg(g_width, "width").required().help("Image width (e.g., 512)") |
+             lyra::arg(g_height, "height").required().help("Image height (e.g., 512)") |
              lyra::arg(vax, "vax").required().help("Vertex 1 x-coordinate (e.g., 100)") |
              lyra::arg(vay, "vay").required().help("Vertex 1 y-coordinate (e.g., 100)") |
              lyra::arg(vbx, "vbx").required().help("Vertex 2 x-coordinate") |
@@ -38,7 +37,7 @@ int main(int argc, char** argv) {
   kiwitracer::BoundingBox bbox = kiwitracer::BoundingBox::calculateBoundingBox(triangle.v1, triangle.v2, triangle.v3);
 
   std::cout << "Filename: " << filename << "\n";
-  std::cout << "Width: " << width << ", Height: " << height << "\n";
+  std::cout << "Width: " << g_width << ", Height: " << g_height << "\n";
   std::cout << "Vertex A: (" << vax << ", " << vay << ")\n";
   std::cout << "Vertex B: (" << vbx << ", " << vby << ")\n";
   std::cout << "Vertex C: (" << vcx << ", " << vcy << ")\n";
@@ -46,15 +45,34 @@ int main(int argc, char** argv) {
             << ")\n";
 
   // Create the image. We're using a `shared_ptr`, a C++11 feature.
-  auto image = std::make_shared<kiwitracer::Image>(width, height);
+  auto image = std::make_shared<kiwitracer::Image>(g_width, g_height);
 
   // Draw the bounding box
-  for (int y = bbox.ymin; y <= bbox.ymax; ++y) {
-    for (int x = bbox.xmin; x <= bbox.xmax; ++x) {
-      // Use a different color for the bounding box (yellow)
-      unsigned char r = 255;
-      unsigned char g = 255;
+  for (float y = bbox.ymin; y <= bbox.ymax; ++y) {
+    for (float x = bbox.xmin; x <= bbox.xmax; ++x) {
+      unsigned char r = 0;
+      unsigned char g = 0;
       unsigned char b = 0;
+      // compute the barycentric coordinates
+      float u =
+          ((vby - vcy) * (x - vcx) + (vcx - vbx) * (y - vcy)) / ((vby - vcy) * (vax - vcx) + (vcx - vbx) * (vay - vcy));
+      float v =
+          ((vbx - vax) * (y - vby) + (vay - vby) * (x - vbx)) / ((vbx - vax) * (vcy - vby) + (vay - vby) * (vcx - vbx));
+      float w = 1.0f - u - v;
+      // if the point is inside the triangle, use the color of the triangle
+      if (u >= 0 && u <= 1 && v >= 0 && v <= 1 && w >= 0 && w <= 1) {
+        r = u * 255;
+        g = v * 255;
+        b = w * 255;
+      }
+
+      if (x == bbox.xmin || x == bbox.xmax || y == bbox.ymin || y == bbox.ymax) {
+        // Use a different color for the bounding box (yellow)
+        r = 255;
+        g = 255;
+        b = 0;
+      }
+
       image->setPixel(x, y, r, g, b);
     }
   }
